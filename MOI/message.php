1ea93ce7d7274b1c4c20218d168466350a5ec6a2
CY TIM <133356013+Neqsu@users.noreply.github.com>
2024-05-26 23:57:41 +0200

    Update message.php

--- MOI/message.php
+++ MOI/message.php
@@ -114,7 +114,7 @@ function getUserProfilePhotoById($id) {
 <html>
 <head>
     <title>Messagerie</title>
-    <link rel="stylesheet" type="text/css" href="messagerie.css">
+    <link rel="stylesheet" type="text/css" href="../styles/messagerie.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>

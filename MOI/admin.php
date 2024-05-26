b620301a89f5a91a14b0b306b400e1c085ac65e3
CY TIM <133356013+Neqsu@users.noreply.github.com>
2024-05-26 19:48:14 +0200

    Update admin.php

--- MOI/admin.php
+++ MOI/admin.php
@@ -86,13 +86,13 @@ function getUsers(){
     <title>Liste des utilisateurs</title>
     <link rel="stylesheet" href="admin.css">
  <title>Infinity'love - RencontreSite</title>
-    <link rel="stylesheet" href="accueil.css">
+    <link rel="stylesheet" href="admin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
 </head>
 
 <body>
-<header>
+    <header>
         <div class="navbar">
             <a href="index.php" class="logo">Infinity Love<span>.</span></a>
             <ul class="menu-links">
@@ -138,7 +138,9 @@ function getUsers(){
         </div>
     </header>
 
+    <div class="title">
     <h1>Liste des utilisateurs</h1>
+    </div>
 <table>
     <thead>
         <tr>

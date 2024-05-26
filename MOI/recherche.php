38bca6ff75cd09ea3e12276f98d40e5cab4a956d
CY TIM <133356013+Neqsu@users.noreply.github.com>
2024-05-26 23:43:13 +0200

    Update recherche.php

--- MOI/recherche.php
+++ MOI/recherche.php
@@ -26,7 +26,7 @@ $droits_utilisateur = $droits[$user_type];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recherche de profils</title>
-    <link rel="stylesheet" href="recherche.css">
+    <link rel="stylesheet" href="../styles/recherche.css">
     <!-- Ajout de la balise de lien pour Font Awesome pour les icônes de réseaux sociaux -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>

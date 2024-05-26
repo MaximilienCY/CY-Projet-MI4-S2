afc33d21d89a295d98b0fb7398e4a629ee410b35
CY TIM <133356013+Neqsu@users.noreply.github.com>
2024-05-26 23:59:42 +0200

    Update mon_profil.php

--- MOI/mon_profil.php
+++ MOI/mon_profil.php
@@ -25,7 +25,7 @@ $droits_utilisateur = $droits[$user_type];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profil de l'utilisateur</title>
-    <link rel="stylesheet" href="mon_profil.css">
+    <link rel="stylesheet" href="../styles/mon_profil.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
